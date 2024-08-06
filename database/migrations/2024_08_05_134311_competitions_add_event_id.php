e73b9807eb06d03c225ad43dfc4cc06fbfde2752
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-08-06 10:00:23 +0700

    feat: add column event_id

---
+++ database/migrations/2024_08_05_134311_competitions_add_event_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->unsignedBigInteger('event_id')->nullable();
+
+            $table->foreign('event_id')->on('events')->references('id')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->dropColumn('event_id');
+        });
+    }
+};
