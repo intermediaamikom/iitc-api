7b23d981b128288a97aa6887a87308713cf8f84a
Didi Prasetyo <didiprasetyo360@gmail.com>
2024-08-08 16:02:02 +0700

    fix: delete hash, hashed by user model

--- app/Http/Controllers/NewPasswordController.php
+++ app/Http/Controllers/NewPasswordController.php
@@ -17,7 +17,7 @@ class NewPasswordController extends Controller
             $request->only('email', 'password', 'token'),
             function (User $user, string $password) {
                 $user->forceFill([
-                    'password' => Hash::make($password)
+                    'password' => $password
                 ])->setRememberToken(Str::random(60));
 
                 $user->save();
